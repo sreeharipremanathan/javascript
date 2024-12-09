ba21ae3411e83b4a639f7e47e6082937161fc480
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-12-09 13:27:51 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,53 @@
+// alert("heyy guys")
+// a="good morning"
+// console.log("welcome all",a)
+// alert(a)
+
+// var a="hey"
+// var a=10
+
+// let a="heyy"
+// a=20
+
+// const a="hey"
+
+
+//data types
+// let a=1.0         //number
+// let a="hello"      //string
+// let a=true         //boolean
+// let a=[1,2,3]        //array
+// let a={name:"abc",age:"18"}     //object
+// let a       //undefined
+
+// console.log(typeof a)
+
+//operators
+//arithmetic
+//+,-,*,/,%,++,--
+// let a=3
+// a++
+// a--
+// console.log(a)
+
+// assignment operators
+//=,+=,-=,*=,/=,%=,**=
+// let a=10
+// a+=5
+// a**=2
+// console.log(a)
+
+// comparison operator
+// ==,!=<,>,<=,>=,===,!==
+
+// let a=10
+// let b="10"
+// console.log(a===b)
+// console.log(a!==b)
+
+//logical operators
+// &&,||
+// let a=10
+// let b="10"
+// console.log(a!=b && a==10)
+// console.log(a!=b ||  a==10)
