85aee5978348433724d39243dfd2a899e3a2becc
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-12-11 09:27:56 +0530

    javascript

--- conditions.js
+++ conditions.js
@@ -33,3 +33,51 @@ function bill(){
         h2.innerHTML="invalid!!"
     }
 }
+
+function tax(){
+    let cost=parseInt(document.getElementById('cost').value)
+    let h2=document.getElementById('display')
+    h2.innerHTML=cost
+    if (cost>100000){
+        h2.innerHTML=cost*0.15
+    }
+    else if (cost>50000 && cost<=100000){
+        h2.innerHTML=cost*0.10
+    }
+    else if (cost<=50000){
+        h2.innerHTML=cost*0.05
+    }
+    else{
+        h2.innerHTML="invalid!!"
+    }
+}
+
+function days(){
+    let day=parseInt(document.getElementById("day").value)
+    let h2=document.getElementById("display")
+
+    if (day==1){
+        h2.innerHTML="sunday"
+    }
+    else if (day==2){
+        h2.innerHTML="monday"
+    }
+    else if (day==3){
+        h2.innerHTML="tuesday"
+    }
+    else if (day==4){
+        h2.innerHTML="wednesday"
+    }
+    else if (day==5){
+        h2.innerHTML="thursday"
+    }
+    else if (day==6){
+        h2.innerHTML="friday"
+    }
+    else if (day==7){
+        h2.innerHTML="saturday"
+    }
+    else{
+        h2.innerHTML="invalid!!"
+    }
+}
